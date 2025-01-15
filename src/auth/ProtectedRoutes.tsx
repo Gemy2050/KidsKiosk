f12b0107ec5f6a9969b373c75c628b2f3f8f6a23
Mohamed <gemy46349@gmail.com>
2025-01-15 04:47:24 +0200

    Fix Protected Routes

--- src/auth/ProtectedRoutes.tsx
+++ src/auth/ProtectedRoutes.tsx
@@ -1,11 +1,12 @@
 import Loader from "@/components/Loader";
 import { useEffect, useState } from "react";
 import useIsAuthenticated from "react-auth-kit/hooks/useIsAuthenticated";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import useCustomQuery from "@/hooks/use-cutstom-query";
 import useAuthUser from "react-auth-kit/hooks/useAuthUser";
 import { User } from "@/types";
 import Cookies from "js-cookie";
+import useSignOut from "react-auth-kit/hooks/useSignOut";
 
 function ProtectedRoutes() {
   const isAuthenticated = useIsAuthenticated();
@@ -13,13 +14,13 @@ function ProtectedRoutes() {
   const [loading, setLoading] = useState(true);
   const isDemo = !!sessionStorage.getItem("isDemo");
 
-  const navigate = useNavigate();
+  const signOut = useSignOut();
 
   const { isSuccess, isError } = useCustomQuery({
     key: ["verify-token"],
     url: `/account/verify-token?isGoogleProvider=${user?.isGoogleUser || ""}`,
     options: {
-      enabled: isAuthenticated,
+      enabled: !!user && isAuthenticated,
       retry: false,
     },
     config: {
@@ -37,13 +38,17 @@ function ProtectedRoutes() {
     if (isError || !isAuthenticated) {
       Cookies.remove("auth");
       Cookies.remove("auth_state");
+      signOut();
       setLoading(false);
-      navigate("/login", { replace: true });
     }
   }, [isSuccess, isError]);
 
   if (loading) {
     return <Loader />;
+  }
+
+  if (!user || isError || !isAuthenticated) {
+    return <Navigate to="/login" replace />;
   }
 
   return <Outlet />;
